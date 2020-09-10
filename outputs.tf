62777960fbb0e0d2a53bca7742e317874347ad79
Tim Arenz <tim@arenz.cc>
2020-09-10 08:59:20 +0200

    Remove encryption_key output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "encryption_key" {
-  description = "Encryption key that was automtacially generated"
-  value       = local.encryption_key
-  sensitive   = true
-}
+# output "encryption_key" {
+#   description = "Encryption key that was automtacially generated"
+#   value       = local.encryption_key
+#   sensitive   = true
+# }
 
 output "id" {
   description = "Output variable to be used for other resources to depend on this module"
